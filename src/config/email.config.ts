a24f50ac1b6eefe5ffdcfb6146b64234c214b4cf
rishir0 <rishiravikumar@icloud.com>
2024-12-23 19:24:02 -0600

    Create email.config.ts

---
+++ src/config/email.config.ts
@@ -0,0 +1,6 @@
+export const EMAIL_CONFIG = {
+  SERVICE_ID: 'service_31mwcna',
+  TEMPLATE_ID: 'template_zcxp3a9',
+  PUBLIC_KEY: 'BGXubkhorwpoqI_9o',
+  TO_EMAIL: 'riseonlinesolutions@gmail.com'
+} as const;
