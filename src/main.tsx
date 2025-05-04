5a03ba1fffc323885750a54eed5faf8482a35ba8
AdityaA26 <a.adityaabhinav10@gmail.com>
2025-05-04 00:56:07 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext'; // Import ThemeProvider
 import './index.css'; // Your global styles
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router> {/* Router wraps AuthProvider and App */}
-      <AuthProvider> {/* AuthProvider wraps App */}
-        <App />
+    <Router>
+      <AuthProvider>
+        <ThemeProvider> {/* Wrap App with ThemeProvider */}
+          <App />
+        </ThemeProvider>
       </AuthProvider>
     </Router>
   </React.StrictMode>,
